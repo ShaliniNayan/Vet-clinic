f06367bd2085910f0e292aed219eb01505cf2393
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2023-07-11 15:16:07 +0530

    Create animal table and entries animal data with queries

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
-/* Database schema to keep the structure of entire database. */
+CREATE DATABASE vet_clinic;
 
-CREATE TABLE animals (
-    name varchar(100)
+create table animals (
+id int GENERATED ALWAYS AS IDENTITY,
+ name varchar(250),
+ date_of_birth date,
+escape_attempts int,
+neutered boolean,
+weight_kg decimal
 );
